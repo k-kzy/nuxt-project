7cf695dbe8f67f5dfc209ecadad3d13e2377d649
k-kzy <rip.to.the.cat+github@gmail.com>
2020-07-27 10:48:10 +0900

    feat：require storybook config file

--- calendar/.storybook/webpack.config.js
+++ calendar/.storybook/webpack.config.js
@@ -4,6 +4,19 @@ const path = require('path')
   (注意) nuxt側とは別で、改めてこちらのwebpack.configを設定する必要がある。
   */
 module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.ts$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+          transpileOnly: true,
+        },
+      },
+    ],
+  })
   config.resolve.alias['@'] = path.resolve(__dirname, '../')
   config.resolve.alias['~'] = path.resolve(__dirname, '../')
   config.module.rules.push({
